eabc9ab6303b3552bc09869073a43bd9aee23176
BalerioGT <elafouibadr@gmail.com>
2024-05-07 19:51:07 +0100

    login form

---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Button from './Button'
+import { facebook, logo1 } from '../assets'
+
+
+const Register = () => {
+    return (
+    <div class="bg-primary w-screen h-screen flex justify-center items-center overflow-scroll">
+      <div class="flex-col justify-center">
+        <div>
+          <img src={logo1} alt="hoobank"/>
+          <h1 class="text-white text-lg text-center mb-5 font-poppins font-bold">Register</h1>
+        </div>
+      <div class="bg-third rounded-md border-[1px] border-gray-600 w-[300px]">
+        <div class="p-5">
+          <label class="text-white font-poppins">Email</label><br></br>
+          <input type="text" class="bg-primary rounded-md mt-2 border-gray-600 border-[1px] w-full"></input>
+        </div>
+        <div class="p-5">
+          <label class="text-white font-poppins">Password</label><br></br>
+          <input type="text" class="bg-primary rounded-md border-gray-600 border-[1px] mt-2 w-full"></input>
+        </div>
+        <div class="p-5 flex justify-center items-center w-full">
+          <Button text="Register"></Button>
+        </div>
+        </div>
+      <div>
+        <div class="rounded-md border-gray-600 border-[1px] mt-5 p-5">
+        </div>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Router,Routes } from 'react-router-dom'
+import Login from './components/Login.jsx'
+import Register from './components/Register.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/register' element={<Register/>}></Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
