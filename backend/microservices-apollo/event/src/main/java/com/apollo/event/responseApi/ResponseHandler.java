6bc14423bea9e67264110ed1865cdd8ffc55c94a
Ettalissimo <oussamaettaouil13@gmail.com>
2024-05-19 17:10:55 +0100

    Add a json response handler (Adapt the Api response format to Event)

---
+++ backend/microservices-apollo/event/src/main/java/com/apollo/event/responseApi/ResponseHandler.java
@@ -0,0 +1,31 @@
+package com.apollo.event.responseApi;
+
+import com.apollo.event.entity.EventEntity;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ResponseHandler {
+    @JsonProperty("count")
+    private int count;
+    @JsonProperty("next")
+    private String next;
+    @JsonProperty("previous")
+    private String previous;
+    @JsonProperty("results")
+    private List<EventEntity> results;
+
+    public int getCount() { return count; }
+    public void setCount(int count) { this.count = count; }
+
+    public String getNext() { return next; }
+    public void setNext(String next) { this.next = next; }
+
+    public String getPrevious() { return previous; }
+    public void setPrevious(String previous) { this.previous = previous; }
+
+    public List<EventEntity> getResults() { return results; }
+    public void setResults(List<EventEntity> results) { this.results = results; }
+}
