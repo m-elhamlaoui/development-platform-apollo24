605210151a241c6e538dcedce10fb5a0cb319cea
Ettalissimo <oussamaettaouil13@gmail.com>
2024-05-19 17:11:26 +0100

    Call External API

--- backend/microservices-apollo/event/src/main/java/com/apollo/event/service/EventService.java
+++ backend/microservices-apollo/event/src/main/java/com/apollo/event/service/EventService.java
@@ -1,11 +1,27 @@
 package com.apollo.event.service;
 
-import com.apollo.event.repository.EventRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.apollo.event.entity.EventEntity;
+import com.apollo.event.responseApi.ResponseHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class EventService {
-    @Autowired
-    private EventRepository eventRepository;
+
+    private String externalApiUrl = "https://lldev.thespacedevs.com/2.2.0/event/";
+
+
+
+    public List<EventEntity> getEventsFromExternalApi() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseHandler response = restTemplate.getForObject(externalApiUrl, ResponseHandler.class);
+        assert response != null;
+        List<EventEntity> events = response.getResults();
+
+        return events;
+    }
 }
