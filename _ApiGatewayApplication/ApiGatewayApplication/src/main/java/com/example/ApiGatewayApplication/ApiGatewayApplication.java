140e149eb4f632c38951bac5be1f732915cbd378
BalerioGT <elafouibadr@gmail.com>
2024-05-23 17:27:56 +0100

    Default gateway routes configurations

--- _ApiGatewayApplication/ApiGatewayApplication/src/main/java/com/example/ApiGatewayApplication/ApiGatewayApplication.java
+++ _ApiGatewayApplication/ApiGatewayApplication/src/main/java/com/example/ApiGatewayApplication/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.example.ApiGatewayApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,23 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder)
+	{
+		return builder.routes()
+				.route("events", p -> p
+						.path("/events")
+						.uri("http://localhost:8080"))
+
+				.route("users",p -> p
+						.path("/users")
+						.uri("http://localhost:8083"))
+
+				.route("model", p -> p
+						.path("/api/v1/ai/generate/info/**")
+						.uri("http://localhost:8081"))
+				.build();
+			}
 }
+
+
